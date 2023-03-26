1422ddd670a54319ec7a1a5ab3adc7847c7f4b2d
Alec <leohfx@gmail.com>
2023-03-26 01:04:40 -0400

    wordcloud command, make error messaages ephemeral

---
+++ commands/wordcloud.js
@@ -0,0 +1,18 @@
+const interactionHandlers = require('../modules/interaction-handlers.js');
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('wordcloud')
+        .setDescription('Generate a colorful wordcloud from your server\'s quotes!')
+        .addIntegerOption(option =>
+            option.setName('size')
+                .setDescription('Size of the wordcloud - 1 (small), 2 (medium), or 3 (large)')
+                .setMinValue(1)
+                .setMaxValue(3)
+                .setRequired(false)
+        ),
+    async execute (interaction) {
+        await interactionHandlers.wordcloudHandler(interaction);
+    }
+};
