549b05b8ceb92a535152c9355ae2a4ddf63d9cc0
Alec <leohfx@gmail.com>
2023-04-11 16:43:21 -0400

    swap position of arguments for add command so to be less confusing

--- commands/add.js
+++ commands/add.js
@@ -6,12 +6,12 @@ module.exports = {
         .setName('add')
         .setDescription('Add a new quote.')
         .addStringOption(option =>
-            option.setName('author')
-                .setDescription('The person(s) who said this quote')
-                .setRequired(true))
-        .addStringOption(option =>
             option.setName('quote')
                 .setDescription('what was said')
+                .setRequired(true))
+        .addStringOption(option =>
+            option.setName('author')
+                .setDescription('The person(s) who said this quote')
                 .setRequired(true)),
     async execute (interaction) {
         await interactionHandlers.addHandler(interaction);
