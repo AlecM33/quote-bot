854b6fcc0e549d1be3860fcf2153ac28879d9bac
Alec <leohfx@gmail.com>
2024-03-11 16:06:15 -0400

    handle when authors message would be too long, fix gap with resolving multiple mentions in one author

--- spec/interaction-handlers-spec.js
+++ spec/interaction-handlers-spec.js
@@ -2,6 +2,7 @@ const interactionHandlers = require('../modules/interaction-handlers.js');
 const queries = require('../database/queries.js');
 const responseMessages = require('../modules/response-messages.js');
 const utilities = require('../modules/utilities.js');
+const constants = require('../modules/constants.js');
 
 describe('interaction handlers', () => {
     describe('#addHandler', () => {
@@ -45,7 +46,7 @@ describe('interaction handlers', () => {
 
             await interactionHandlers.addHandler(interaction);
 
-            expect(queries.addQuote).toHaveBeenCalledWith('quote', 'author', '123');
+            expect(queries.addQuote).toHaveBeenCalledWith('quote', 'author', '123', undefined);
             expect(interaction.reply).toHaveBeenCalledWith('Added the following:\n\n_"test"_ - jane doe');
         });
 
@@ -128,6 +129,111 @@ describe('interaction handlers', () => {
 
             expect(queries.getQuotesFromAuthor).toHaveBeenCalledWith('jane', '123');
             expect(interaction.reply).toHaveBeenCalled();
+        });
+    });
+
+    describe('#authorsHandler', () => {
+        let interaction;
+
+        beforeAll(() => {
+            interaction = {
+                options: {
+                    getString: (string) => {
+                        if (string === 'author') {
+                            return null;
+                        }
+                    }
+                },
+                deferReply: async () => { this.deferred = true; },
+                guildId: '123',
+                followUp: async (arg) => {}
+            };
+
+            spyOn(interaction, 'deferReply');
+            spyOn(interaction, 'followUp');
+        });
+
+        it('should follow up with a list of authors separated by bullets', async () => {
+            spyOn(queries, 'fetchUniqueAuthors').and.returnValue([
+                {
+                    author: 'jack'
+                },
+                {
+                    author: 'jane'
+                }
+            ]);
+
+            await interactionHandlers.authorsHandler(interaction);
+
+            expect(queries.fetchUniqueAuthors).toHaveBeenCalledWith('123');
+            expect(interaction.followUp).toHaveBeenCalledWith('Here are all the different authors, separated by ' +
+                'bullet points: \n\n' + 'jack' + constants.AUTHOR_SEPARATOR + 'jane');
+        });
+
+        it('should attach a file if the authors message would be too long', async () => {
+            spyOn(queries, 'fetchUniqueAuthors').and.returnValue([
+                {
+                    author: 'a'.repeat(500)
+                },
+                {
+                    author: 'b'.repeat(500)
+                },
+                {
+                    author: 'c'.repeat(500)
+                },
+                {
+                    author: 'd'.repeat(500)
+                }
+            ]);
+
+            await interactionHandlers.authorsHandler(interaction);
+
+            expect(queries.fetchUniqueAuthors).toHaveBeenCalledWith('123');
+            expect(interaction.followUp).toHaveBeenCalledWith(jasmine.objectContaining(
+                {
+                    content: 'Here are all the different authors. There are a lot of them, so I put them in the attached text file.'
+                }
+            ));
+        });
+
+        it('should attach a file if the authors message would be too long, formatting authors if necessary', async () => {
+            spyOn(queries, 'fetchUniqueAuthors').and.returnValue([
+                {
+                    author: 'a'.repeat(2000)
+                },
+                {
+                    author: '<@121212121212121212>'
+                },
+                {
+                    author: 'myself and <@121212121212121212>'
+                },
+                {
+                    author: '<#121212121212121212> and myself and <@121212121212121212> and <@&121212121212121212>'
+                },
+                {
+                    author: '@myself'
+                },
+                {
+                    author: '<343434343434>'
+                }
+            ]);
+            spyOn(utilities, 'formatAuthor').and.callFake(async (guildManager, interaction, author) => {
+                return await author;
+            });
+
+            await interactionHandlers.authorsHandler(interaction, { });
+
+            expect(queries.fetchUniqueAuthors).toHaveBeenCalledWith('123');
+            expect(utilities.formatAuthor).toHaveBeenCalledWith(jasmine.anything(), jasmine.anything(), '<@121212121212121212>');
+            expect(utilities.formatAuthor).toHaveBeenCalledWith(jasmine.anything(), jasmine.anything(), 'myself and <@121212121212121212>');
+            expect(utilities.formatAuthor).toHaveBeenCalledWith(jasmine.anything(), jasmine.anything(), '<#121212121212121212> and myself and <@121212121212121212> and <@&121212121212121212>');
+            expect(utilities.formatAuthor).not.toHaveBeenCalledWith(jasmine.anything(), jasmine.anything(), '@myself');
+            expect(utilities.formatAuthor).not.toHaveBeenCalledWith(jasmine.anything(), jasmine.anything(), '<343434343434>');
+            expect(interaction.followUp).toHaveBeenCalledWith(jasmine.objectContaining(
+                {
+                    content: 'Here are all the different authors. There are a lot of them, so I put them in the attached text file.'
+                }
+            ));
         });
     });
 });
